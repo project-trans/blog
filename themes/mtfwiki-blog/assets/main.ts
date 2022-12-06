b3b7a9b6b0ab2cd2a2ca05e8155471cef1735fb0
Septs <github@septs.pw>
2022-12-06 17:40:17 +0800

    Regular update: 2022-12-06T09:40:17Z

--- themes/mtfwiki-blog/assets/main.ts
+++ themes/mtfwiki-blog/assets/main.ts
@@ -1,8 +1,35 @@
-const matched = matchMedia('(prefers-color-scheme: dark)')
+const ready = new Promise<void>((resolve) => {
+  if (document.readyState === 'interactive') {
+    return resolve()
+  }
+  document.addEventListener('readystatechange', (event) => {
+    if (document.readyState === 'interactive') {
+      resolve()
+    }
+  })
+})
 
-setTheme(matched.matches)
-matched.addEventListener('change', (event) => setTheme(event.matches))
+ready.then(onTheme).then(onTableResponsive)
 
-function setTheme(matches: boolean) {
-  document.documentElement.dataset.theme = matches ? 'dark' : 'light'
+function onTheme() {
+  const matched = matchMedia('(prefers-color-scheme: dark)')
+
+  matched.addEventListener('change', setTheme)
+
+  setTheme(matched)
+
+  function setTheme(event: { matches: boolean }) {
+    document.documentElement.dataset.theme = event.matches ? 'dark' : 'light'
+  }
+}
+
+function onTableResponsive() {
+  const content = document.querySelector('.content')
+  if (content === null) return
+  for (let element of content.getElementsByTagName('table')) {
+    const responsive = document.createElement('section')
+    responsive.className = 'table-responsive'
+    responsive.append(element.cloneNode(true))
+    element.replaceWith(responsive)
+  }
 }
